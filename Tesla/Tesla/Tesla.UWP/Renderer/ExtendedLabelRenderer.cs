793fa7f0cd7a2ab9c721a90a65d910691ec2872a
Adam Pedley <adam.pedley@gmail.com>
2016-03-09 19:30:53 +1100

    Keypad Gesture Recognizer

---
+++ Tesla/Tesla/Tesla.UWP/Renderer/ExtendedLabelRenderer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesla.Gestures;
+using Tesla.UWP.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Label), typeof(ExtendedLabelRenderer))]
+namespace Tesla.UWP.Renderer
+{
+
+    public class ExtendedLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.PointerPressed += Control_PointerPressed;
+                Control.PointerReleased += Control_PointerReleased;
+            }
+        }
+
+        private void Control_PointerReleased(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            foreach (var recognizer in this.Element.GestureRecognizers)
+            {
+                var gesture = recognizer as PressedGestureRecognizer;
+                if (gesture != null)
+                    if (gesture.Command != null)
+                        gesture.Command.Execute(this.Element);
+            }
+        }
+
+        private void Control_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            foreach (var recognizer in this.Element.GestureRecognizers)
+            {
+                var gesture = recognizer as ReleasedGestureRecognizer;
+                if (gesture != null)
+                    if (gesture.Command != null)
+                        gesture.Command.Execute(this.Element);
+            }
+        }
+    }
+}
--- Tesla/Tesla/Tesla/Control/Keypad.cs
+++ Tesla/Tesla/Tesla/Control/Keypad.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Exrin.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tesla.Gestures;
 using Xamarin.Forms;
 
 namespace Tesla.Control
@@ -12,7 +14,6 @@ namespace Tesla.Control
 
         public Keypad()
         {
-
             this.ColumnSpacing = 3;
             this.RowSpacing = 3;
 
@@ -41,12 +42,75 @@ namespace Tesla.Control
             var textColor = Color.White;
 
             for (int i = 0; i < 9; i++)
-                this.Children.Add(new Label() { BackgroundColor = backgroundColor, TextColor = textColor, Text = (i + 1).ToString(), HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center }, i - ((i / 3) * 3), i / 3);
+            {
+                var label = new Label() { HorizontalOptions= LayoutOptions.FillAndExpand, VerticalOptions=LayoutOptions.FillAndExpand, BackgroundColor = backgroundColor, TextColor = textColor, Text = (i + 1).ToString(), HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center };
+                label.GestureRecognizers.Add(new TapGestureRecognizer() { Command = ButtonTap, CommandParameter = label });
+                label.GestureRecognizers.Add(new PressedGestureRecognizer() { Command = PressedCommand, CommandParameter = label });
+                label.GestureRecognizers.Add(new ReleasedGestureRecognizer() { Command = ReleasedCommand, CommandParameter = label });
+                this.Children.Add(label, i - ((i / 3) * 3), i / 3);
+            }
 
             this.Children.Add(new Label() { BackgroundColor = backgroundColor, TextColor = textColor, Text = "", HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center }, 0, 3);
             this.Children.Add(new Label() { BackgroundColor = backgroundColor, TextColor = textColor, Text = "0", HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center }, 1, 3);
             this.Children.Add(new Label() { BackgroundColor = backgroundColor, TextColor = textColor, Text = "<", HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center }, 2, 3);
         }
 
+        private RelayCommand _buttonTap = null;
+        private RelayCommand ButtonTap
+        {
+            get
+            {
+                if (_buttonTap == null)
+                    _buttonTap = new RelayCommand((parameter) =>
+                    {
+                        //var label = parameter as Label;
+
+                        //label.Animate("Tap", new Animation());
+
+                       
+
+                    });
+
+                return _buttonTap;
+            }
+        }
+
+        private RelayCommand _pressedCommand = null;
+        private RelayCommand PressedCommand
+        {
+            get
+            {
+                if (_pressedCommand == null)
+                    _pressedCommand = new RelayCommand((parameter) =>
+                    {
+                        var label = parameter as Label;
+                        label.BackgroundColor = Color.Black;
+                        label.TextColor = Color.White;
+                    });
+
+                return _pressedCommand;
+            }
+
+        }
+
+        private RelayCommand _releasedCommand = null;
+        private RelayCommand ReleasedCommand
+        {
+            get
+            {
+                if (_releasedCommand == null)
+                    _releasedCommand = new RelayCommand((parameter) =>
+                    {
+                        var label = parameter as Label;
+                        label.BackgroundColor = Color.White;
+                        label.TextColor = Color.Black;
+                    });
+
+                return _releasedCommand;
+            }
+
+        }
+
+       
     }
 }
