31f58e3a4c5f9c04e33818711f8f7b8f70bc558d
Adam Pedley <adam.pedley@gmail.com>
2016-03-30 12:05:03 +1100

    CommonService

---
+++ Tesla/Tesla/Tesla.Tests/CommonService.cs
@@ -0,0 +1,29 @@
+﻿using Exrin.Abstraction;
+using Exrin.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesla.Tests
+{
+    public class CommonService
+    {
+        public static IDisplayService DisplayService
+        {
+            get
+            {
+                return new DisplayService();
+            }
+        }
+
+        public static IErrorHandlingService ErrorHandlingService
+        {
+            get
+            {
+                return new ErrorHandlingService();
+            }
+        }
+    }
+}
