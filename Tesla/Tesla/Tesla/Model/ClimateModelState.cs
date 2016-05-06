10d7cfda092e68e8e83d564dbbebeb9afee5761e
adampedley1 <adam.pedley@gmail.com>
2016-05-07 00:29:58 +1000

    Minor Bug fix

--- Tesla/Tesla/Tesla/Model/ClimateModelState.cs
+++ Tesla/Tesla/Tesla/Model/ClimateModelState.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeslaDefinition.Interfaces.Model;
 
 namespace Tesla.Model
 {
-    public class ClimateModelState: ModelState
+    public class ClimateModelState: ModelState, IClimateModelState
     {
 		public double Temperature { get { return Get<double>(); } set { Set(value); } }
 	}
