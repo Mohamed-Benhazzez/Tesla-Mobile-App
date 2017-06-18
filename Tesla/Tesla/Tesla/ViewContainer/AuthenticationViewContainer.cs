5a7d72e98dc1db64666743846a8573025a0b8d7a
Adam Pedley <adam.pedley@gmail.com>
2017-06-18 14:12:48 +1000

    Update to latest Exrin version

--- Tesla/Tesla/Tesla/ViewContainer/AuthenticationViewContainer.cs
+++ Tesla/Tesla/Tesla/ViewContainer/AuthenticationViewContainer.cs
@@ -1,17 +1,20 @@
 ﻿namespace Tesla.ViewContainer
 {
-    using Exrin.Abstraction;
-    using Proxy;
-    using Stack;
-    using TeslaDefinition.Enums;
+	using System;
+	using Exrin.Abstraction;
+	using Proxy;
+	using Stack;
+	using TeslaDefinition.Enums;
+	using Xamarin.Forms;
 
-    public class AuthenticationViewContainer : Exrin.Framework.ViewContainer, IMasterDetailContainer
+	public class AuthenticationViewContainer : Exrin.Framework.ViewContainer, IMasterDetailContainer
     {
-        
-        public AuthenticationViewContainer(AuthenticationStack stack, BookingStack bookingStack) 
+		private readonly MasterDetailPage _page = new MasterDetailPage();
+
+		public AuthenticationViewContainer(AuthenticationStack stack, BookingStack bookingStack) 
             : base(ViewContainers.Authentication.ToString())
         {
-            var mdp = new MasterDetailProxy(new Xamarin.Forms.MasterDetailPage());
+            var mdp = new MasterDetailProxy(_page);
             NativeView = mdp.View;
             Proxy = mdp;
             DetailStack = stack;
@@ -23,5 +26,20 @@
         public IStack MasterStack { get; set; }
 
         public IMasterDetailProxy Proxy { get; set; }
-    }
+
+		public bool IsPresented { get { return _page.IsPresented; } set { _page.IsPresented = value; } }
+
+		public void SetStack(ContainerType type, object page)
+		{
+			switch (type)
+			{
+				case ContainerType.Detail:
+					_page.Detail = page as Page;
+					break;
+				case ContainerType.Master:
+					_page.Master = page as Page;
+					break;
+			}
+		}
+	}
 }
